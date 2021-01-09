17ff8cee345da703e359d9881a70357b60947c45
EpsteinRoss-FS <repstein@student.fullsail.edu>
2021-01-08 20:24:22 -0500

    Initial Commit

---
+++ Epstein_Ross_Inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Epstein_Ross_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
