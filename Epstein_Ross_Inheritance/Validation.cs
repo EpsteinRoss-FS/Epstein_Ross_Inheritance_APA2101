3e89c866d57f5e2a23f6e98035629776a8c6b799
EpsteinRoss-FS <repstein@student.fullsail.edu>
2021-01-08 20:33:57 -0500

    added validation for string and int

--- Epstein_Ross_Inheritance/Validation.cs
+++ Epstein_Ross_Inheritance/Validation.cs
@@ -6,5 +6,17 @@ namespace Epstein_Ross_Inheritance
 {
     class Validation
     {
+
+        public static bool ValidateString(string validateString) 
+        {
+            bool stringValid = validateString.Length >= 1 ? true : false;
+            return stringValid;
+        }
+
+        public static bool CheckInt(string intCheck)
+        {
+            bool isItInt = int.TryParse(intCheck, out _);
+            return (isItInt);
+        }
     }
 }
